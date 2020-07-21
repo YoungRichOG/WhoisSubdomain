847c64372e702230e13d6d1ffea26aaa95f24504
YoungRichOG <3093556545@qq.com>
2020-07-22 01:33:06 +0800

    优化输出

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ if __name__ == '__main__':
 				get_contacts_info = chinaz.get_contacts_info(contacts=get_whois[1])
 				get_email_info = chinaz.get_email_info(email=get_whois[0])
 				get_icp_info = chinaz.get_icp_info(domain=i.rstrip())
+				print('[*] get_contacts_info模块:',get_contacts_info)
+				print('[*] get_email_info模块:',get_email_info)
+				print('[*] get_icp_info模块:',get_icp_info)
 				new_list = get_email_info + get_contacts_info + get_icp_info
 				count = 0
 				for s in list(set(new_list)):
